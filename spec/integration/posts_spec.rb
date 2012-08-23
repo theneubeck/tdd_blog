298d77ca5e87ac723a0d238c3be924c23d3adcd3
Jens Carlen <jens@neubeck.se>
2012-08-23 15:57:34 +0200

    Added clearifying comments

--- spec/integration/posts_spec.rb
+++ spec/integration/posts_spec.rb
@@ -1,10 +1,17 @@
-describe "Writing a post", :type => :request do
+describe "Blogging", :type => :request do
 
-  it "should write a post" do
-    blog_post = FactoryGirl.create(:post, :title => "Hello World!", :body=> "My first post")
-    visit "/"
-    page.should have_content("Hello World!")
-    page.should have_content("My first post")
+  context "the blog" do
+
+    it "should show a post" do
+      # Given a blog post
+      blog_post = FactoryGirl.create(:post, :title => "Hello World!", :body=> "My first post")
+      # When I visit "/"
+      visit "/"
+      # Then I should see "Hello World!"
+      page.should have_content("Hello World!")
+      page.should have_content("My first post")
+    end
+    
   end
 end
 
